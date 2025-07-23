403a9d44ac1917c76421201508c2949ded7c4979
User <119732698+andrewnms@users.noreply.github.com>
2025-07-24 03:57:52 +0800

    fix: Changed sizing subheader

--- components/landingHire/sections/3rdSection/feature-section.tsx
+++ components/landingHire/sections/3rdSection/feature-section.tsx
@@ -41,7 +41,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
         <h2 className="text-6xl md:text-8xl mb-4 text-black dark:text-white max-w-4xl">
           Everything you need in One Platform.
         </h2>
-        <p className="text-neutral-700 dark:text-neutral-300 text-xl">
+        <p className="text-neutral-700 dark:text-neutral-300 text-3xl">
           All-in-one internship hiring platform. Simple. Fast. Seamless.
         </p>
       </div>
@@ -73,7 +73,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
           style={{
             height: height + "px",
           }}
-          className="absolute md:left-8 left-8 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
+          className="absolute md:left-8 left-8 top-0 w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 dark:via-neutral-700 to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
         >
           <motion.div
             style={{
