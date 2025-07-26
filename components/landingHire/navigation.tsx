32cb8913f32df0334ad1e43ae0953ea3b06f9c3a
neue-dev <modavid.1964@gmail.com>
2025-07-26 12:12:49 +0800

    hotfix: update useRouter import

--- components/landingHire/navigation.tsx
+++ components/landingHire/navigation.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { UserCircle } from "lucide-react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export function Navigation() {
   const router = useRouter();
