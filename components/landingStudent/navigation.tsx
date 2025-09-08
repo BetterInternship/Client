a965e083a2591c8dc522c1976b55c87eedca54f8
Jana Marie Bantolino <bantolino.jana@gmail.com>
2025-09-08 23:19:55 +0800

    chore: better navbar on welcome screen

--- components/landingStudent/navigation.tsx
+++ components/landingStudent/navigation.tsx
@@ -3,15 +3,15 @@
 export function Navigation() {
   return (
     <nav>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-16">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-12 sm:h-16">
           <div className="flex items-center gap-2">
             <img
               src="/BetterInternshipLogo.png"
-              className="w-8 h-8 inline-block"
+              className="inline-block h-7 w-7 sm:h-8 sm:w-8"
               alt="BetterInternship logo"
             />
-            <div className="text-2xl font-bold font-primary tracking-tight">
+            <div className="font-primary font-bold tracking-tight text-lg sm:text-2xl">
               BetterInternship
             </div>
           </div>
