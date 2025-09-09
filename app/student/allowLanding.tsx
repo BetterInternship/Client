dc0c85f2bb2bdea0d574dd90fd452c398bc5e828
neue-dev <modavid.1964@gmail.com>
2025-09-09 09:48:44 +0800

    fix header useSearchParams()

--- app/student/allowLanding.tsx
+++ app/student/allowLanding.tsx
@@ -1,21 +1,23 @@
-"use client"
+"use client";
 
 import { usePathname } from "next/navigation";
 import Header from "@/components/features/student/header";
 import { Footer } from "@/components/shared/footer";
+import { Suspense } from "react";
 
-export default function AllowLanding({ children }: { children: React.ReactNode }) {
+export default function AllowLanding({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const pathname = usePathname();
   const isStudentLanding = pathname === "/";
 
   return (
     <div className="h-screen bg-gray-50 flex flex-col">
-      {!isStudentLanding && <Header />}
-      <div className="flex-grow overflow-auto flex flex-col">
-        {children}
-      </div>
+      <Suspense>{!isStudentLanding && <Header />}</Suspense>
+      <div className="flex-grow overflow-auto flex flex-col">{children}</div>
       {!isStudentLanding && <Footer />}
     </div>
   );
 }
-
