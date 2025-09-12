b18674648eb9780e20461e9cfea4b1232c12ba2e
neue-dev <modavid.1964@gmail.com>
2025-09-12 20:29:34 +0800

    hotfix: remove debug bg color

--- components/features/student/header.tsx
+++ components/features/student/header.tsx
@@ -603,7 +603,7 @@ export const Header = () => {
           )}
           {isMobile && (
             <div className="flex flex-col max-w-2xl w-full gap-4 items-center">
-              <div className="relative max-w-2xl w-full border bg-red-500 border-gray-300 rounded-[0.33em] hover:pointer-cursor ">
+              <div className="relative max-w-2xl w-full border border-gray-300 rounded-[0.33em] hover:pointer-cursor ">
                 <Search className="absolute left-4 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400 pointer-events-none" />
                 <input
                   type="text"
