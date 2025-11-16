79013b11dd975ef2ce32695085d2b95df3d80fd4
chua-e <159678059+chua-e@users.noreply.github.com>
2025-11-16 15:57:13 +0800

    better flow for no job listings

--- components/features/hire/dashboard/JobsContent.tsx
+++ components/features/hire/dashboard/JobsContent.tsx
@@ -2,9 +2,12 @@
 
 "use client";
 
-import { Badge } from "@/components/ui/badge";
+import { Plus } from "lucide-react";
 import { EmployerApplication, Job } from "@/lib/db/db.types";
 import { JobListingsBox } from "./JobListingsBox";
+import { useAppContext } from "@/lib/ctx-app";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 //maybe add employers id to cross check
 interface JobsContentProps {
@@ -22,31 +25,48 @@ export function JobsContent({
     onJobListingClick,
     updateJob
 }: JobsContentProps) {
+    const { isMobile } = useAppContext();
     const sortedJobs = jobs.sort(
         (a,b) => 
        ((b.created_at ?? "") > (a.created_at ?? "")) ? 1 : -1
     );
 
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-2">
+        <>
             {jobs.length > 0
                 ?
                 (
-                    sortedJobs.filter(job => job.employer_id === employerId
-                    ).map((job) => (
-                        <JobListingsBox
-                            key={job.id}
-                            job={job}
-                            applications={applications}
-                            onJobListingClick={onJobListingClick}
-                            update_job={updateJob}
-                        />
-                    ))
+                    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-2">
+                        {
+                            sortedJobs.filter(job => job.employer_id === employerId
+                            ).map((job) => (
+                                <JobListingsBox
+                                    key={job.id}
+                                    job={job}
+                                    applications={applications}
+                                    onJobListingClick={onJobListingClick}
+                                    update_job={updateJob}
+                                />
+                            ))
+                        }
+                    </div>
                 ) : (
-                    <div className="p-2 m-4">
-                        <Badge>No job listings currently.</Badge>
+                    <div className={cn("grid place-items-center mt-[25vh]",
+                        isMobile ? "" : "gap-4"
+                    )}>
+                        <p className="text-gray-500 text-lg">No job listings currently.</p>
+                        {isMobile ? (
+                            <p className="text-sm text-gray-400">Start creating jobs using the + button below.</p>
+                        ):(
+                            <Link href="listings/create">
+                                <button className="flex text-primary rounded-sm p-4 gap-2 items-center border hover:opacity-60">
+                                    <Plus className="h-5 w-5"/>
+                                    <p className="text-primary text-sm">Start by creating a listing</p>
+                                </button>
+                            </Link>
+                        )}
                     </div>
                 )}
-        </div>
+        </>
     );
 }
