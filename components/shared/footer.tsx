561a5d816cc52eff26c4a5ffdc9f0231066e318e
Jana Marie Bantolino <bantolino.jana@gmail.com>
2025-11-17 15:46:57 +0800

    chore: better mobile footer

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -20,15 +20,13 @@ export function Footer({ links = LINKS }: { links?: FooterLink[] }) {
   return (
     <footer className="py-1 px-5">
       <div className="mx-auto">
-        <div className="flex gap-2 justify-center md:flex-row md:items-center md:justify-between">
-          <p className="text-xs text-muted-foreground text-center md:text-left">
+        <div className="flex gap-2 justify-end md:flex-row md:items-center md:justify-between">
+          <p className="text-xs text-muted-foreground text-center md:text-left hidden md:block">
             © {year} BetterInternship Inc.
           </p>
 
-          <p className="text-xs md:hidden text-muted-foreground">•</p>
-
           <nav aria-label="Footer" className="flex justify-center">
-            <ul className="flex flex-wrap items-center gap-x-2 md:gap-x-4 gap-y-2 text-xs">
+            <ul className="flex flex-wrap gap-x-2 md:gap-x-4 gap-y-2 text-xs">
               {links.map((l) => (
                 <li key={l.label}>
                   <Link
