fed13dcb548b44cc07e6d5703588b7dbd143d933
Janica Reyes <janicareyes6@gmail.com>
2025-12-04 03:23:36 +0800

    test: combined student.search and student.forcredit; adding before each to the test files to simplify the test cases

--- cypress/e2e/student.profiledropdown.cy.ts
+++ cypress/e2e/student.profiledropdown.cy.ts
@@ -1,70 +1,48 @@
-it("Visit Student Profile and check clickable tabs available", () => {
-  // This handles the authentication; if u wanna use another account, just replace the email in the link
-  cy.visit(
-    "http://localhost:5000/api/auth/google/cypress/modavid.1964@gmail.com",
-  );
+describe("Using the Search or Filter", () => {
+  
+  beforeEach(() => {
+    // This handles the authentication; if u wanna use another account, just replace the email in the link
+    cy.visit(
+      "http://localhost:5000/api/auth/google/cypress/modavid.1964@gmail.com",
+    );
 
-  // This happens automatically, don't change this line
-  cy.url().should("contain", "preload");
+    // This happens automatically, don't change this line
+    cy.url().should("contain", "preload");
 
-  cy.contains("Malks Mogen David").click();
-  cy.contains("Profile").click();
-  cy.contains("Internship Details").click();
-  cy.contains("Type of internship");
-  cy.contains("Preferences");
+    cy.contains("Malks Mogen David").click();
+  });
 
-  cy.contains("Student Profile").click();
-  cy.contains("Identity");
-  cy.contains("Educational Background");
-  cy.contains("External Profiles");
-  cy.contains("Personal Bio");
+  it("Visit Student Profile and check clickable tabs available", () => {
+    cy.contains("Profile").click();
+    cy.contains("Internship Details").click();
+    cy.contains("Type of internship");
+    cy.contains("Preferences");
 
-});
+    cy.contains("Student Profile").click();
+    cy.contains("Identity");
+    cy.contains("Educational Background");
+    cy.contains("External Profiles");
+    cy.contains("Personal Bio");
 
-it("Visit Student Applications and check clickable tabs available", () => {
-  // This handles the authentication; if u wanna use another account, just replace the email in the link
-  cy.visit(
-    "http://localhost:5000/api/auth/google/cypress/modavid.1964@gmail.com",
-  );
+  });
 
-  // This happens automatically, don't change this line
-  cy.url().should("contain", "preload");
+  it("Visit Student Applications and check the job applied", () => {
+    cy.contains("Applications").click();
 
-  cy.contains("Malks Mogen David").click();
-  cy.contains("Applications").click();
+    cy.contains("Operations Intern");
+    cy.contains("View Details").click();
+    cy.contains("Description");
+    cy.contains("Applied");
 
-  cy.contains("Operations Intern");
-  cy.contains("View Details").click();
-  cy.contains("Applied").click();
+  });
 
-});
+  it("Visit Student Saved Jobs", () => {
+    cy.contains("Saved Jobs").click();
+    cy.contains("Saved Jobs")
+  });
 
-it("Visit Student Saved Jobs and check clickable tabs available", () => {
-  // This handles the authentication; if u wanna use another account, just replace the email in the link
-  cy.visit(
-    "http://localhost:5000/api/auth/google/cypress/modavid.1964@gmail.com",
-  );
-
-  // This happens automatically, don't change this line
-  cy.url().should("contain", "preload");
-
-  cy.contains("Malks Mogen David").click();
-  cy.contains("Saved Jobs").click();
-
-});
-
-it("Visit Student Forms and check clickable tabs available", () => {
-  // This handles the authentication; if u wanna use another account, just replace the email in the link
-  cy.visit(
-    "http://localhost:5000/api/auth/google/cypress/modavid.1964@gmail.com",
-  );
-
-  // This happens automatically, don't change this line
-  cy.url().should("contain", "preload");
-
-  cy.contains("Malks Mogen David").click();
-  cy.contains("Forms").click();
-
-  cy.contains("Forms").click();
-  cy.contains("Form Generator").click();
+  it("Visit Student Forms", () => {
+    cy.contains("Forms").click();
+    cy.contains("Forms");
+  });
 });
--- cypress/e2e/student.search.cy.ts
+++ cypress/e2e/student.search.cy.ts
@@ -1,15 +1,48 @@
-it("Bring the user to the Internship Dashboard and search for Intestship", function () {
-  cy.visit("http://localhost:3000/");
+describe("Using the Search or Filter", () => {
+  
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
 
-  cy.contains("Find Internships").click();
+    cy.contains("Find Internships").click();
 
-  cy.url().should("include", "/search");
+    cy.url().should("include", "/search");
+  });
 
-  cy.get('input[placeholder="Search Internship Listings"]').type(
-    "cypress test job listing{enter}",
-  );
+  it("Search for a job", function () {
+    cy.get('input[placeholder="Search Internship Listings"]').type(
+      "cypress test job listing{enter}",
+    );
 
-  cy.contains("cypress test job listing").should("exist").click();
+    cy.contains("cypress test job listing").should("exist").click();
 
-  cy.get("div.flex-1").should("contain", "cypress test job listing");
+    cy.get("div.flex-1").should("contain", "cypress test job listing");
+  });
+  
+  it("Student filters for 'For Credit' and verifies DLSU MOA badge on all results", () => {
+  cy.visit("http://localhost:3000/search");
+
+  // click Filter
+  cy.contains("button", "FOR CREDIT").click();
+
+  cy.url().should("include", "moa=Has+MOA");
+
+  // wait for results (at least one badge is visible)
+  cy.contains("DLSU MOA", { timeout: 10000 }).should("be.visible");
+
+  // check if all visible cards have the badge
+  cy.get('button').filter(':contains("Apply Now")')
+    .should('have.length.gt', 0) 
+    .each(($btn, index) => {
+      cy.log(`Checking Card #${index + 1}`); 
+
+      cy.wrap($btn)
+        .parents()
+        .filter((i, el) => Cypress.$(el).text().includes("DLSU MOA"))
+        .first()
+        .should('exist')
+        .within(() => {
+            cy.contains("DLSU MOA").should("be.visible");
+        });
+    });
+  });
 });
