a25ee04585c7530b911ff78d7abf3d1663cd2da6
Janica Reyes <janicareyes6@gmail.com>
2025-12-04 00:10:45 +0800

    test: edited the test cases for profile dropdown

--- cypress/e2e/student.profiledropdown.cy.ts
+++ cypress/e2e/student.profiledropdown.cy.ts
@@ -19,12 +19,6 @@ it("Visit Student Profile and check clickable tabs available", () => {
   cy.contains("External Profiles");
   cy.contains("Personal Bio");
 
-
-  cy.contains("Malks Mogen David").click();
-  cy.contains("Saved Jobs").click();
-
-  cy.contains("Malks Mogen David").click();
-  cy.contains("Forms").click();
 });
 
 it("Visit Student Applications and check clickable tabs available", () => {
@@ -39,9 +33,38 @@ it("Visit Student Applications and check clickable tabs available", () => {
   cy.contains("Malks Mogen David").click();
   cy.contains("Applications").click();
 
+  cy.contains("Operations Intern");
+  cy.contains("View Details").click();
+  cy.contains("Applied").click();
+
+});
+
+it("Visit Student Saved Jobs and check clickable tabs available", () => {
+  // This handles the authentication; if u wanna use another account, just replace the email in the link
+  cy.visit(
+    "http://localhost:5000/api/auth/google/cypress/modavid.1964@gmail.com",
+  );
+
+  // This happens automatically, don't change this line
+  cy.url().should("contain", "preload");
+
   cy.contains("Malks Mogen David").click();
   cy.contains("Saved Jobs").click();
 
+});
+
+it("Visit Student Forms and check clickable tabs available", () => {
+  // This handles the authentication; if u wanna use another account, just replace the email in the link
+  cy.visit(
+    "http://localhost:5000/api/auth/google/cypress/modavid.1964@gmail.com",
+  );
+
+  // This happens automatically, don't change this line
+  cy.url().should("contain", "preload");
+
   cy.contains("Malks Mogen David").click();
   cy.contains("Forms").click();
+
+  cy.contains("Forms").click();
+  cy.contains("Form Generator").click();
 });
