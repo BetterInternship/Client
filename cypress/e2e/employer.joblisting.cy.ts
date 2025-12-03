0e4477bcbab8969ee21eaec5dfa2b8e8d714d4f7
Janica Reyes <janicareyes6@gmail.com>
2025-12-04 01:03:19 +0800

    test: created more test cases for createjob, deletejob, job listing; adjusted student.apply

--- cypress/e2e/employer.joblisting.cy.ts
+++ cypress/e2e/employer.joblisting.cy.ts
@@ -1,12 +1,38 @@
-it("Employer checks the applicants of a job listing", function () {
+it("Employer checks the applicants of cypress test job listing 1", function () {
   cy.loginAsEmployer();
 
 
   cy.visit("http://hire.localhost:3000/dashboard");
   cy.contains("Job listings").click();
-  cy.contains("cypress test job listing").click();
+  cy.contains("cypress test job listing 1").click();
   cy.contains("Applicant").parent().parent().within(() => {
     cy.contains("Janica Megan Reyes");
+  });
+
+});
+
+it("Employer checks the applicants of cypress 2", function () {
+  cy.loginAsEmployer();
+
+
+  cy.visit("http://hire.localhost:3000/dashboard");
+  cy.contains("Job listings").click();
+  cy.contains("cypress 2").click();
+  cy.contains("Applicant").parent().parent().within(() => {
+    cy.contains("Janica Megan Reyes");
+  });
+
+});
+
+it("Employer checks the applicants of cypress 3", function () {
+  cy.loginAsEmployer();
+
+
+  cy.visit("http://hire.localhost:3000/dashboard");
+  cy.contains("Job listings").click();
+  cy.contains("cypress 3").click();
+  cy.contains("Applicant").parent().parent().within(() => {
+    cy.contains("Janica Megan Reyes").should("not.exist");
   });
 
 });
--- cypress/e2e/student.apply.cy.ts
+++ cypress/e2e/student.apply.cy.ts
@@ -18,7 +18,7 @@ it("Student tries to find a job listing; will fail if search didnt show the job 
   
 });
 
-it("Student has to click the left panel to show the job listing searched; apply but cancel", () => {
+it("Student has to click the left panel to show the job listing searched; apply now but cancel", () => {
   // This handles the authentication; if u wanna use another account, just replace the email in the link
   cy.visit(
     "http://localhost:5000/api/auth/google/cypress/janica_megan_reyes@dlsu.edu.ph",
@@ -30,10 +30,10 @@ it("Student has to click the left panel to show the job listing searched; apply 
   cy.get('input[placeholder="Search Internship Listings"]').type(
     "cypress test job listing{enter}",
   );
-  cy.contains("cypress test job listing").click();
+  cy.contains("cypress test job listing 1").click();
 
   cy.contains("Job Details").parent().parent().within(() => {
-    cy.contains("cypress test job listing");
+    cy.contains("cypress test job listing 1");
     cy.contains("Apply Now").click();
   });
     cy.contains("Cancel").click();
@@ -41,7 +41,7 @@ it("Student has to click the left panel to show the job listing searched; apply 
 
 });
 
-it("Student tries to apply and submit", () => {
+it("Student tries to apply to cypress test job listing 1 and submit application", () => {
   
   cy.visit(
     "http://localhost:5000/api/auth/google/cypress/janica_megan_reyes@dlsu.edu.ph",
@@ -51,20 +51,20 @@ it("Student tries to apply and submit", () => {
   cy.url().should("contain", "preload");
 
   cy.get('input[placeholder="Search Internship Listings"]').type(
-    "cypress test job listing{enter}",
+    "cypress test job listing 1{enter}",
   );
   //make sure we are applying to the right job
-  cy.contains("cypress test job listing").click();
+  cy.contains("cypress test job listing 1").click();
 
   cy.contains("Job Details").parent().parent().within(() => {
-    cy.contains("cypress test job listing");
+    cy.contains("cypress test job listing 1");
     cy.contains("Apply Now").click();
   });
-  cy.contains("Submit Application").should("be.enabled");
-  
+  cy.contains("Submit Application").click();
+  cy.contains("Application Sent!");
 });
 
-it("Student saves the job then uses the apply now button", () => {
+it("Student saves the cypress 2 then uses the apply now button", () => {
   
   cy.visit(
     "http://localhost:5000/api/auth/google/cypress/janica_megan_reyes@dlsu.edu.ph",
@@ -74,22 +74,23 @@ it("Student saves the job then uses the apply now button", () => {
   cy.url().should("contain", "preload");
 
   cy.get('input[placeholder="Search Internship Listings"]').type(
-    "cypress test job listing{enter}",
+    "cypress 2{enter}",
   );
   //make sure we are applying to the right job
-  cy.contains("cypress test job listing").click();
+  cy.contains("cypress 2").click();
 
   cy.contains("Job Details").parent().parent().within(() => {
-    cy.contains("cypress test job listing");
+    cy.contains("cypress 2");
     cy.contains("Save").click();
     cy.contains("Saved");
   });
   cy.contains("Janica Megan Reyes").click();
   cy.contains("Saved Jobs").click();
-  cy.contains("cypress test job listing").parent().parent().within(() => {
+  cy.contains("cypress 2").parent().parent().within(() => {
     cy.contains("View Details").click();
   });
   cy.contains("Saved");
   cy.contains("Apply Now").click();
   cy.contains("Submit Application").click();
+  cy.contains("Application Sent!");
 });
