fd6150292f834c60372beff2628e631b187fe23c
Janica Reyes <janicareyes6@gmail.com>
2025-12-04 05:15:14 +0800

    test: create more test cases for job listing

--- cypress/e2e/employer.joblisting.cy.ts
+++ cypress/e2e/employer.joblisting.cy.ts
@@ -20,22 +20,47 @@ describe("Employer Job Listing Flows", () => {
     const jobId = "fedb61ca-77fe-49f0-b76a-5435abc5896e"; 
     cy.visit(`http://hire.localhost:3000/dashboard/manage?jobId=${jobId}`);
   });
+  
+    it("Visit Listing Information tab then return to Applicants", () => {
+      cy.contains("Listing information").click();
+      cy.contains("Job Details");
+      cy.contains("Applicants").click();
+      cy.contains("Applicant");
+    });
 
-  it("Filters applicants by status (Shortlisted, Accepted, Rejected)", () => {
-    // click shortlisted and make sure the list updates to show them
-    cy.contains("button", "Shortlisted").click();
-    cy.contains("Shortlisted", { timeout: 10000 }).should("be.visible");
+  // it("Filters applicants by status (Shortlisted, Accepted, Rejected)", () => {
+  //   // click shortlisted and make sure the list updates to show them
+  //   cy.contains("button", "Shortlisted").click();
+  //   cy.contains("Shortlisted", { timeout: 10000 }).should("be.visible");
 
-    // do the same for accepted applicants
-    cy.contains("button", "Accepted").click();
-    cy.contains("Accepted").should("be.visible");
+  //   // do the same for accepted applicants
+  //   cy.contains("button", "Accepted").click();
+  //   cy.contains("Accepted").should("be.visible");
 
-    // check if the rejected filter works too
-    cy.contains("button", "Rejected").click();
-    cy.contains("Rejected").should("be.visible");
+  //   // check if the rejected filter works too
+  //   cy.contains("button", "Rejected").click();
+  //   cy.contains("Rejected").should("be.visible");
 
-    // click 'll to reset everything and show the whole list again
-    cy.contains("button", "All").click();
-    cy.contains("Applicant").should("be.visible"); 
+  //   // click 'll to reset everything and show the whole list again
+  //   cy.contains("button", "All").click();
+  //   cy.contains("Applicant").should("be.visible"); 
+  // });
+
+
+  it("Change Job Listing Status to paused", () => {
+    cy.contains("Active").parent().within(() => {
+    cy.get("button").click()
   });
+    cy.contains("Paused").click()
+  });
+
+  it("Change Job Listing Status to Active again", () => {
+    
+    cy.contains("Paused").parent().within(() => {
+    cy.get("button").click()
+  });
+    cy.contains("Active").click()
+  });
+
+  
 });
