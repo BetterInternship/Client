e54cae5b41dd39ffc99800c618405335637744a7
Janica Reyes <janicareyes6@gmail.com>
2025-12-03 22:18:29 +0800

    test: added test cases for employerlogin.cy.ts

--- cypress/e2e/Employerlogin.cy.ts
+++ cypress/e2e/Employerlogin.cy.ts
@@ -2,6 +2,24 @@ it("prevents invalid credentials from logging in", () => {
   cy.visit("http://hire.localhost:3000/login");
 
   cy.get('input[type="email"]').type("fakeemail@gmail.com");
+  cy.get('button[type="submit"]').click();
+
+  cy.contains("Invalid password.");
+});
+
+it("prevents invalid credentials from logging in", () => {
+  cy.visit("http://hire.localhost:3000/login");
+
+  cy.get('input[type="password"]').type("fakepassword");
+  cy.get('button[type="submit"]').click();
+
+  cy.contains("Email is required.");
+});
+
+it("prevents invalid credentials from logging in", () => {
+  cy.visit("http://hire.localhost:3000/login");
+
+  cy.get('input[type="email"]').type("fakeemail@gmail.com");
   cy.get('input[type="password"]').type("fakepassword");
   cy.get('button[type="submit"]').click();
 
