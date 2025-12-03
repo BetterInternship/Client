b0a9e969544a84404e5d194c4064832cde1f612f
neue-dev <modavid.1964@gmail.com>
2025-12-03 20:50:02 +0800

    add comments to student.forms.cy.ts

--- cypress/e2e/student.forms.cy.ts
+++ cypress/e2e/student.forms.cy.ts
@@ -1,9 +1,16 @@
 it("prevents invalid credentials from logging in", () => {
+  // This handles the authentication; if u wanna use another account, just replace the email in the link
   cy.visit(
     "http://localhost:5000/api/auth/google/cypress/modavid.1964@gmail.com",
   );
+
+  // This happens automatically, don't change this line
   cy.url().should("contain", "preload");
-  cy.url().should("contain", "search");
-  cy.visit("http://localhost:3000/forms");
-  cy.contains("Forms");
+
+  // !
+  // !
+  // You can change the tests below this line
+  cy.url().should("contain", "search"); // Check if on search page (this is where user shud be after log in)
+  cy.visit("http://localhost:3000/forms"); // Visit user forms page
+  cy.contains("Forms"); // Check if on forms page
 });
