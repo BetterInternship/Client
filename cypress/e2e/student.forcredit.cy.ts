5e9c13512fbbc1c64313d9651d3e7dde04a5df5d
Kyle Bibon <bibonkyle25@gmail.com>
2025-12-04 01:46:39 +0800

    more tests

---
+++ cypress/e2e/student.forcredit.cy.ts
@@ -0,0 +1,27 @@
+it("Student filters for 'For Credit' and verifies DLSU MOA badge on all results", () => {
+  cy.visit("http://localhost:3000/search");
+
+  // click Filter
+  cy.contains("button", "FOR CREDIT").click();
+
+  cy.url().should("include", "moa=Has+MOA");
+
+  // wait for results (at least one badge is visible)
+  cy.contains("DLSU MOA", { timeout: 10000 }).should("be.visible");
+
+  // check if all visible cards have the badge
+  cy.get('button').filter(':contains("Apply Now")')
+    .should('have.length.gt', 0) 
+    .each(($btn, index) => {
+      cy.log(`Checking Card #${index + 1}`); 
+
+      cy.wrap($btn)
+        .parents()
+        .filter((i, el) => Cypress.$(el).text().includes("DLSU MOA"))
+        .first()
+        .should('exist')
+        .within(() => {
+            cy.contains("DLSU MOA").should("be.visible");
+        });
+    });
+});
