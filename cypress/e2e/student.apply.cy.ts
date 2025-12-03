52390a09bacba44455241c0325e0fc992329f688
Janica Reyes <janicareyes6@gmail.com>
2025-12-04 02:37:56 +0800

    test: uncomment testcases

--- cypress/e2e/student.apply.cy.ts
+++ cypress/e2e/student.apply.cy.ts
@@ -1,45 +1,45 @@
-// it("Student tries to find a job listing; will fail if search didnt show the job in the main panel", () => {
-//   // This handles the authentication; if u wanna use another account, just replace the email in the link
-//   cy.visit(
-//     "http://localhost:5000/api/auth/google/cypress/janica_megan_reyes@dlsu.edu.ph",
-//   );
+it("Student tries to find a job listing; will fail if search didnt show the job in the main panel", () => {
+  // This handles the authentication; if u wanna use another account, just replace the email in the link
+  cy.visit(
+    "http://localhost:5000/api/auth/google/cypress/janica_megan_reyes@dlsu.edu.ph",
+  );
 
-//   // This happens automatically, don't change this line
-//   cy.url().should("contain", "preload");
+  // This happens automatically, don't change this line
+  cy.url().should("contain", "preload");
 
-//   cy.get('input[placeholder="Search Internship Listings"]').type(
-//     "cypress test job listing{enter}",
-//   );
+  cy.get('input[placeholder="Search Internship Listings"]').type(
+    "cypress test job listing{enter}",
+  );
 
-//   cy.contains("Job Details").parent().parent().within(() => {
-//     cy.contains("cypress test job listing");
-//     cy.contains("Apply Now").should("exist");
-//   });
+  cy.contains("Job Details").parent().parent().within(() => {
+    cy.contains("cypress test job listing");
+    cy.contains("Apply Now").should("exist");
+  });
   
-// });
+});
 
-// it("Student has to click the left panel to show the job listing searched; apply now but cancel", () => {
-//   // This handles the authentication; if u wanna use another account, just replace the email in the link
-//   cy.visit(
-//     "http://localhost:5000/api/auth/google/cypress/janica_megan_reyes@dlsu.edu.ph",
-//   );
+it("Student has to click the left panel to show the job listing searched; apply now but cancel", () => {
+  // This handles the authentication; if u wanna use another account, just replace the email in the link
+  cy.visit(
+    "http://localhost:5000/api/auth/google/cypress/janica_megan_reyes@dlsu.edu.ph",
+  );
 
-//   // This happens automatically, don't change this line
-//   cy.url().should("contain", "preload");
+  // This happens automatically, don't change this line
+  cy.url().should("contain", "preload");
 
-//   cy.get('input[placeholder="Search Internship Listings"]').type(
-//     "cypress test job listing{enter}",
-//   );
-//   cy.contains("cypress test job listing 1").click();
+  cy.get('input[placeholder="Search Internship Listings"]').type(
+    "cypress test job listing{enter}",
+  );
+  cy.get("div.flex-1").contains("cypress test job listing 1").click();
 
-//   cy.contains("Job Details").parent().parent().within(() => {
-//     cy.contains("cypress test job listing 1");
-//     cy.contains("Apply Now").click();
-//   });
-//     cy.contains("Cancel").click();
-//     cy.contains("Submit Application").should("not.exist");
+  cy.contains("Job Details").parent().parent().within(() => {
+    cy.contains("cypress test job listing 1");
+    cy.contains("Apply Now").click();
+  });
+    cy.contains("Cancel").click();
+    cy.contains("Submit Application").should("not.exist");
 
-// });
+});
 
 it("Student tries to apply to cypress test job listing 1 and submit application", () => {
   
@@ -54,7 +54,7 @@ it("Student tries to apply to cypress test job listing 1 and submit application"
     "cypress test job listing 1{enter}",
   );
   //make sure we are applying to the right job
-  cy.contains("cypress test job listing 1").click();
+  cy.get("div.flex-1").contains("cypress test job listing 1").click();
 
   cy.contains("Job Details").parent().parent().within(() => {
     cy.contains("cypress test job listing 1");
