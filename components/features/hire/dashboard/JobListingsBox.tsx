12ffa467ace8218c9e084cc88d4edb0dc5da71cb
Jay Carlos <jaymichael_carlos@icloud.com>
2025-12-07 18:11:37 +0800

    feat: add button to center of dashboard when no listings are present

--- components/features/hire/dashboard/JobListingsBox.tsx
+++ components/features/hire/dashboard/JobListingsBox.tsx
@@ -13,17 +13,12 @@ import { useRouter } from "next/navigation";
 interface JobListingProps {
     job: Job;
     applications: EmployerApplication[];
-    update_job: (
-        job_id: string,
-        job: Partial<Job>,
-    ) => Promise<{ success: boolean }>;
     isLoading?: boolean;
 }
 
 export function JobListingsBox({
     job,
     applications,
-    update_job,
     isLoading
 }: JobListingProps) {
     const { to_job_pay_freq_name } = useDbRefs();
@@ -40,7 +35,6 @@ export function JobListingsBox({
     };
 
     const router = useRouter();
-    const isMobile = useMobile();
 
     if(isLoading) {
         return (
