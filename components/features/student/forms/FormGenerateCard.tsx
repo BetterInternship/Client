1b363ce020b99bc32cdaafda09687218cf8e4c2f
Jana Marie Bantolino <bantolino.jana@gmail.com>
2026-01-06 17:40:40 +0800

    feat: improve layout and styling of FormTemplateCard component

--- components/features/student/forms/FormGenerateCard.tsx
+++ components/features/student/forms/FormGenerateCard.tsx
@@ -11,9 +11,9 @@ export default function FormTemplateCard({
 }) {
   return (
     <div className="border-b hover:bg-gray-100 duration-150 transition-all">
-      <Card className="flex flex-row justify-end items-center p-2 rounded-none border-none bg-transparent">
+      <Card className="flex flex-col sm:flex-row sm:justify-between sm:items-center p-0 py-2 sm:p-3 rounded-none border-none bg-transparent ">
         <div className="flex gap-0 w-full overflow-hidden">
-          <div className="px-3 py-2 text-gray-700 text-sm text-ellipsis text-nowrap line-clamp-1">
+          <div className=" sm:px-3 py-1 text-gray-700 text-xs sm:text-sm break-words sm:text-ellipsis sm:text-nowrap sm:line-clamp-1">
             {formLabel}
           </div>
         </div>
@@ -22,10 +22,10 @@ export default function FormTemplateCard({
           onClick={onGenerate}
           variant="outline"
           scheme="primary"
-          className="flex flex-row justify-between bg-primary/10 border-none "
+          className="flex flex-row justify-between gap-1 sm:gap-2 bg-primary/10 border-none flex-shrink-0 text-xs sm:text-sm h-8 sm:h-9 w-full sm:w-auto"
         >
           Open
-          <SquareArrowOutUpRight />
+          <SquareArrowOutUpRight className="w-4 h-4" />
         </Button>
       </Card>
     </div>
