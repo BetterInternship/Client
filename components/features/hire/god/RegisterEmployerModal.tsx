d1ad26d74a4420baa2d355c3fb5a3a47372a3a3c
neue-dev <modavid.1964@gmail.com>
2026-01-28 13:37:33 +0800

    feat: magic link gen

--- components/features/hire/god/RegisterEmployerModal.tsx
+++ components/features/hire/god/RegisterEmployerModal.tsx
@@ -12,6 +12,7 @@ import { FormInput } from "@/components/EditForm";
 import { isValidEmail } from "@/lib/utils";
 import { APIClient, APIRouteBuilder } from "@/lib/api/api-client";
 import { FetchResponse } from "@/lib/api/use-fetch";
+import { Autocomplete } from "@/components/ui/autocomplete";
 
 export type ModalRendererProps = {
   children?: React.ReactNode;
@@ -20,7 +21,6 @@ export type ModalRendererProps = {
 };
 
 export function RegisterEmployerButton({ onOpen }: { onOpen: () => void }) {
-  useEffect(() => console.debug("[RegisterEmployerButton] mounted"), []);
   return (
     <Button
       scheme="supportive"
@@ -30,6 +30,19 @@ export function RegisterEmployerButton({ onOpen }: { onOpen: () => void }) {
       }}
     >
       Register new
+    </Button>
+  );
+}
+
+export function GenerateMagicLinkButton({ onOpen }: { onOpen: () => void }) {
+  return (
+    <Button
+      scheme="supportive"
+      onClick={() => {
+        onOpen();
+      }}
+    >
+      Generate magic link
     </Button>
   );
 }
@@ -108,9 +121,86 @@ export function RegisterEmployerModal({
               <Button
                 scheme="supportive"
                 disabled={loading}
-                onClick={handleRegister}
+                onClick={() => void handleRegister()}
               >
                 {!loading ? "Register" : "Registering..."}
+              </Button>
+              <Button variant="outline" onClick={onClose}>
+                Cancel
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
+}
+
+export function GenerateMagicLinkModal({
+  Modal,
+  onClose,
+  employers,
+}: {
+  Modal: React.ComponentType<ModalRendererProps>;
+  onClose: () => void;
+  employers: { id: string; name: string }[];
+}) {
+  const [selectedEid, setSelectedEid] = useState("");
+  const [magicLink, setMagicLink] = useState(
+    "https://hire.betterinternship.com/yes",
+  );
+  const [loading, setLoading] = useState(false);
+
+  const handleGen = async () => {
+    if (!selectedEid) return alert("Select employer first.");
+    setLoading(true);
+    const url = APIRouteBuilder("employer")
+      .r("god", "generate-magic-link")
+      .build();
+    const res = await APIClient.post<FetchResponse & { magicLink: string }>(
+      url,
+      {
+        employer_id: selectedEid,
+      },
+    );
+    if (res.success) {
+      setMagicLink(res.magicLink);
+    } else {
+      alert("Error:" + res.message);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <Modal>
+      <div className="flex flex-col items-center justify-center p-8 pt-0 w-[100vw]">
+        <div className="w-full h-96">
+          <h1 className="font-heading font-bold text-2xl mb-4 text-gray-700">
+            Generate a Magic Link for an Employer
+          </h1>
+          <div className="flex flex-col gap-20 w-96">
+            <Autocomplete
+              label="Company"
+              options={employers}
+              value={selectedEid}
+              setter={(value) => setSelectedEid(value!)}
+            />
+            <div
+              className="bg-gray-100 p-5 rounded-[0.33em] hover:cursor-pointer text-ellipsis line-clamp-1 text-nowrap text-xs"
+              onClick={() => {
+                void navigator.clipboard.writeText(magicLink);
+                alert("Copied!");
+              }}
+            >
+              <code>{magicLink}</code>
+            </div>
+            <div className="mt-2 flex gap-2">
+              <Button
+                scheme="supportive"
+                disabled={loading}
+                onClick={() => void handleGen()}
+              >
+                {!loading ? "Generate" : "Generating..."}
               </Button>
               <Button variant="outline" onClick={onClose}>
                 Cancel
