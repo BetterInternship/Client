e99466ba7d769facd7cfe502fe43877bf685f7da
Jana Marie Bantolino <bantolino.jana@gmail.com>
2026-02-04 15:32:25 +0800

    chore: make share button same size

--- components/features/student/job/share-job-button.tsx
+++ components/features/student/job/share-job-button.tsx
@@ -1,44 +1,35 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Check, Share } from "lucide-react";
 import { useEffect, useState } from "react";
 
-export const ShareJobButton = ({id} : {id: string}) => {
+export const ShareJobButton = ({ id }: { id: string }) => {
   const [clicked, setClicked] = useState(false);
-  
+
   const copyJobLink = () => {
-    navigator.clipboard.writeText(`${process.env.NEXT_PUBLIC_CLIENT_URL}/search/${id}`);
+    navigator.clipboard.writeText(
+      `${process.env.NEXT_PUBLIC_CLIENT_URL}/search/${id}`,
+    );
     setClicked(true);
     setTimeout(() => setClicked(false), 1500);
-  }
+  };
 
-  useEffect(
-    () => {
-      setClicked(false);
-    }, [id]
-  );
-  
+  useEffect(() => {
+    setClicked(false);
+  }, [id]);
+
   return (
     <Button
-      variant="link"
+      variant="ghost"
       onClick={copyJobLink}
       name="Copy link"
       scheme="default"
-      size="sm"
-      className={cn(
-        "text-xs",
-        clicked ? "text-supportive" : "text-gray-500"
-      )}
+      size="md"
+      className={cn("!p-4", clicked ? "text-supportive" : "text-gray-500")}
     >
-      {clicked
-        ? <Check />
-        : <Share />
-      }
-      {clicked
-        ? "Copied link"
-        : "Share"
-      }
+      {clicked ? <Check /> : <Share />}
+      {clicked ? "Copied link" : "Share"}
     </Button>
   );
 };
