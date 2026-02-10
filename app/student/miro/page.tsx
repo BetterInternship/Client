cca0fb4533af90f5e0559cf6b3528bfd1df25d28
Jana Marie Bantolino <bantolino.jana@gmail.com>
2026-02-11 00:21:03 +0800

    chore: put correct eventStart

--- app/student/miro/page.tsx
+++ app/student/miro/page.tsx
@@ -293,7 +293,7 @@ function RemoteCursorsDemo() {
 export default function MiroThonLandingPage() {
   const discordLink = "https://discord.gg/QZ9mXJQm";
 
-  const eventStart = useMemo(() => new Date("2026-02-09T18:00:00+08:00"), []);
+  const eventStart = useMemo(() => new Date("2026-02-13T18:00:00+08:00"), []);
   const eventEnd = useMemo(() => new Date("2026-02-14T23:59:00+08:00"), []);
 
   const [countdown, setCountdown] = useState<Countdown>(
