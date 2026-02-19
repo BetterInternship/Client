877df078b3f0a3deeb67aaf40776fa8cd1b226fa
anaj00 <jana_bantolino@dlsu.edu.ph>
2026-02-19 10:48:27 +0800

    chore: read info from the right place

--- components/features/student/forms/form-renderer.ctx.tsx
+++ components/features/student/forms/form-renderer.ctx.tsx
@@ -162,14 +162,14 @@ export const FormRendererContextProvider = ({
     const fm = new FormMetadata(form.formMetadata);
     const newFormName = form.formMetadata.name;
     const newFormLabel = form.formMetadata.label;
-    const newFormVersion = form.formDocument.version;
+    const newFormVersion = form.formTemplate.version;
 
     // Only update form if it's new
     setFormMetadata(fm);
     setFormName(newFormName);
     setFormLabel(newFormLabel);
     setFormVersion(newFormVersion);
-    setDocumentName(form.formDocument.name);
+    setDocumentName(form.formMetadata.label);
     setDocumentUrl(form.documentUrl);
     const loadedFields = fm.getFieldsForClientService("initiator");
 
