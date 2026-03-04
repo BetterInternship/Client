55a406dea35da4e190a1a61d25d1988407a3f7ac
jayylmao <73204320+jayylmao@users.noreply.github.com>
2026-03-04 20:23:43 +0800

    feat: currency formatting
    Added a util function to format currency properly

--- lib/utils/num-utils.ts
+++ lib/utils/num-utils.ts
@@ -10,3 +10,20 @@ export const isValidOptionalPhoneNumber = (phone: string): boolean => {
   const cleanPhone = phone.replace(/\D/g, "");
   return cleanPhone.length === 11 && /^09\d{9}$/.test(cleanPhone);
 };
+
+/**
+ * Formats a number as currency.
+ */
+export const formatCurrency = (
+  value: number,
+  locale = "en-PH",
+  currency = "PHP",
+) => {
+  if (value === null || Number.isNaN(value)) return "N/A";
+
+  return new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency,
+    maximumFractionDigits: 2,
+  }).format(value);
+};
