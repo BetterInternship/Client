7fb8525009a37fa67b2939e9db15eeaaa7f038d0
neue-dev <modavid.1964@gmail.com>
2026-03-06 13:09:09 +0800

    patch: export state record contract

--- hooks/base/useStateRecord.tsx
+++ hooks/base/useStateRecord.tsx
@@ -1,7 +1,7 @@
 /**
  * @ Author: BetterInternship
  * @ Create Time: 2026-03-04 14:26:13
- * @ Modified time: 2026-03-04 14:47:51
+ * @ Modified time: 2026-03-06 13:00:11
  * @ Description:
  *
  * This seems to be a very common use case.
@@ -10,9 +10,19 @@
 
 import { useState } from "react";
 
+export type StateRecord<T = string> = Record<string, T>;
+
+export interface StateRecordActions<T = string> {
+  setOne: (key: string, value: T) => void;
+  setMany: (newRecord: Record<string, T>) => void;
+  clearOne: (key: string) => void;
+  clearAll: () => void;
+  overwrite: (newRecord: Record<string, T>) => void;
+}
+
 export const useStateRecord = <T = string,>(
   initialState: Record<string, T>,
-) => {
+): [Record<string, T>, StateRecordActions<T>] => {
   const [record, setRecord] = useState<Record<string, T>>(initialState);
 
   const setOne = (key: string, value: T) => {
