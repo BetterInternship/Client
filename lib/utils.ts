48cb35a1ef6ebd4fa989e7fef9f0827220e9972c
jayylmao <73204320+jayylmao@users.noreply.github.com>
2026-08-11 17:21:33 +0800

    chore: rm local cn string util

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,10 +2,3 @@ export * from "./utils/string-utils";
 export * from "./utils/date-utils";
 export * from "./utils/num-utils";
 export * from "./utils/file-utils";
-
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
